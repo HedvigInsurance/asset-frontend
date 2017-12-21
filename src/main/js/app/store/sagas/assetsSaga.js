5b36109ca9c60f499c76858b8b2a4c6894ac64c3
Trofimov Pavel <pavtrof342@gmail.com>
2017-12-21 15:57:25 +0500

    Feature/asset list (#5)
    * assets list & asset card ui-components
    * update request body
    * asset date fix
    * asset date fix
    * assets update state

--- src/main/js/app/store/sagas/assetsSaga.js
+++ src/main/js/app/store/sagas/assetsSaga.js
@@ -1,4 +1,4 @@
-import { call, put, takeLatest } from 'redux-saga/effects';
+import { call, put, takeLatest, select } from 'redux-saga/effects';
 import api from '../../api/AssetApi';
 
 import { ASSET_UPDATING, ASSET_REQUESTING } from '../constants/actionTypes';
@@ -13,8 +13,15 @@ import {
 function* assetUpdateFlow(action) {
     try {
         const { assetId, assetState } = action;
-        const updatedWidget = yield call(api.update, assetId, assetState);
-        yield put(assetUpdateSuccess(updatedWidget));
+        const { data } = yield call(api.update, assetId, assetState);
+        const { assets: { list } } = yield select();
+        const updatedList = list.map(asset => {
+            if (asset.id === data.id) {
+                return {...asset, state: data.state};
+            }
+            return asset;
+        });
+        yield put(assetUpdateSuccess(updatedList));
     } catch (error) {
         yield put(assetUpdateError(error));
     }
